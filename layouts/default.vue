3c05f6a01736736de61082f5ceff01d216654f32
kunado <katsusand1@gmail.com>
2018-11-18 06:25:49 +0900

    cut out the header component from the default template

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,6 @@
 <template>
   <div>
-    <header class="globalHeader">
-        <div class="headerLogo">
-            <a href="/" class="homeLink">TaskDriver</a>
-        </div>
-        <nav class="headerNav">
-            <ul>
-                <li><a href="#">タスク一覧</a></li>
-                <li><a href="#">活動サマリ</a></li>
-                <li><a href="#">グループ</a></li>
-                <li><a href="#">プロフィール</a></li>
-                <li><a href="#">ログアウト</a></li>
-            </ul>
-        </nav>
-    </header>
+    <Header/>
     <article>
       <div class="container">
         <nuxt/>
@@ -23,36 +10,17 @@
 </template>
 
 <style lang="scss" scoped>
-    .globalHeader {
-        background-color: $red;
-        padding: 12px 30px;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        .homeLink {
-            color: $white;
-            font-weight: 800;
-            font-size: 30px;
-            text-decoration: none;
-        }
-        .headerNav {
-            ul {
-                display: flex;
-                list-style: none;
-                li {
-                    a {
-                        color: $white;
-                        text-decoration: none;
-                        margin: 3px;
-                        font-size: 15px;
-                        font-weight: 600;
-                    }
-                }
-            }
-        }
-    }
     .container {
         width: 1000px;
         margin: auto;
     }
 </style>
+
+<script>
+    import Header from '~/components/molecules/Header.vue'
+    export default {
+        components: {
+            Header
+        }
+    }
+</script>
