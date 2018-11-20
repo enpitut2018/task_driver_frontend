be348758c3953e9da19452403f02c6967ec284b2
chr <stchr820.6281.kkt@gmail.com>
2018-11-20 18:08:04 +0900

    create HeaderUnLoggedIn.vue

---
+++ components/organisms/HeaderUnLoggedIn.vue
@@ -0,0 +1,53 @@
+<template>
+	<header>
+		<div class="headerLogo">
+			<a href="/" class="homeLink">Folivora</a>
+		</div>
+		<nav class="headerNav">
+			<ul>
+                <li><a href="#"><button>ログイン</button></a></li>
+                <li><a href="#"><button>ユーザ登録</button></a></li>
+			</ul>
+		</nav>
+	</header>
+</template>
+
+<style lang="scss" scoped>
+	button {
+		@include buttonReflect($startButtonColor, $white);
+		font-weight: 500;
+		font-size: 14px;
+		letter-spacing: .1em;
+		padding: 8px 20px;
+		border-radius: 6px;
+		border: none;
+	}
+	header {
+		background-color: $red;
+		padding: 12px 30px;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		.homeLink {
+			color: $white;
+			font-weight: 800;
+			font-size: 30px;
+			text-decoration: none;
+		}
+		.headerNav {
+			ul {
+				display: flex;
+				list-style: none;
+				li {
+					a {
+						color: $white;
+						text-decoration: none;
+						margin: 3px;
+						font-size: 15px;
+						font-weight: 600;
+					}
+				}
+			}
+		}
+	}
+</style>
---
+++ layouts/unLoggedIn.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <HeaderUnLoggedIn/>
+    <article>
+      <div class="container">
+        <nuxt/>
+      </div>
+    </article>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+    .container {
+        width: 1000px;
+        margin: auto;
+    }
+</style>
+
+<script>
+    import HeaderUnLoggedIn from '~/components/organisms/HeaderUnLoggedIn.vue'
+    export default {
+        components: {
+            HeaderUnLoggedIn
+        }
+    }
+</script>
