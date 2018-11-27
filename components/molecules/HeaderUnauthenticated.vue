05fc7df7bdd20e08ee481908e04491fd936f9bed
kunado <katsusand1@gmail.com>
2018-11-28 07:32:59 +0900

    use env var for parameters in login

--- components/molecules/HeaderUnauthenticated.vue
+++ components/molecules/HeaderUnauthenticated.vue
@@ -65,8 +65,8 @@ export default {
   			await this.$auth.loginWith('local', {
   				data: {
   					user: {
-  						email: 's1611442@u.tsukuba.ac.jp',
-  						password: 'password'
+  						email: process.env.EMAIL,
+  						password: process.env.PASSWORD
   					}
   				}
   			});
