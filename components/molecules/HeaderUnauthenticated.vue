c5f207d6a4fe195b5a2dd857a26aa32d7f62f042
kunado <katsusand1@gmail.com>
2018-12-02 16:07:40 +0900

    insert footer to layouts and modify styles

--- components/molecules/HeaderUnauthenticated.vue
+++ components/molecules/HeaderUnauthenticated.vue
@@ -1,55 +1,62 @@
 <template>
 	<header>
-		<div class="headerLogo">
-			<a href="/" class="homeLink">Folivora</a>
+		<div class="container">
+			<div class="headerLogo">
+				<a href="/" class="homeLink">Folivora</a>
+			</div>
+			<nav class="headerNav">
+				<ul>
+	                <li><button class="loginButton" @click="authenticate">ログイン</button></li>
+	                <li><a href="#"><button class="registerButton">ユーザ登録</button></a></li>
+				</ul>
+			</nav>
 		</div>
-		<nav class="headerNav">
-			<ul>
-                <li><button class="loginButton" @click="authenticate">ログイン</button></li>
-                <li><a href="#"><button class="registerButton">ユーザ登録</button></a></li>
-			</ul>
-		</nav>
 	</header>
 </template>
 
 <style lang="scss" scoped>
-	button {
-		font-weight: 500;
-		font-size: 14px;
-		letter-spacing: .1em;
-		padding: 8px 20px;
-		border-radius: 6px;
-		border: none;
-    	&.loginButton {
-        	@include buttonReflectWithBorder($red, $white);
-    	}
-    	&.registerButton {
-        	@include buttonReflectWithBorder($startButtonColor, $white);
-    	}
-	}
 	header {
 		background-color: $red;
 		padding: 12px 30px;
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-		.homeLink {
-			color: $white;
-			font-weight: 800;
-			font-size: 30px;
-			text-decoration: none;
-		}
-		.headerNav {
-			ul {
-				display: flex;
-				list-style: none;
-				li {
-					a {
-						color: $white;
-						text-decoration: none;
-						margin: 3px;
-						font-size: 15px;
-						font-weight: 600;
+		.container {
+			width: 1000px;
+			margin: auto;
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+			.homeLink {
+				color: $white;
+				font-weight: 500;
+				font-size: 35px;
+				text-decoration: none;
+			}
+			.headerNav {
+				ul {
+					display: flex;
+					list-style: none;
+					li {
+						a {
+							color: $white;
+							text-decoration: none;
+							margin: 3px;
+							font-size: 15px;
+							font-weight: 600;
+						}
+						button {
+							font-weight: 500;
+							font-size: 14px;
+							letter-spacing: .1em;
+							padding: 8px 20px;
+							border-radius: 6px;
+							margin: 0 3px;
+							border: none;
+					    	&.loginButton {
+					        	@include buttonReflectWithBorder($red, $white);
+					    	}
+					    	&.registerButton {
+					        	@include buttonReflectWithBorder($startButtonColor, $white);
+					    	}
+						}
 					}
 				}
 			}
