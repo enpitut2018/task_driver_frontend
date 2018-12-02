deaee56cc77b101ec3d95d9450bba067e0bebc47
kunado <katsusand1@gmail.com>
2018-12-03 06:17:52 +0900

    create login page

--- pages/login.vue
+++ pages/login.vue
@@ -1,28 +1,17 @@
 <template>
   <div>
-    <h1>Hello World</h1>
-    <button @click="authenticate">Login</button>
+    <LoginForm />
   </div>
 </template>
 
 <script>
-export default {
-  methods: {
-  	async authenticate () {
-  		try {
-  			await this.$auth.loginWith('local', {
-  				data: {
-  					user: {
-  						email: 's1611442@u.tsukuba.ac.jp',
-  						password: 'password'
-  					}
-  				}
-  			})
-  			this.$router.push('/login')
-  		} catch (e) {
-  			this.error = true
-  		}
-  	}
-  },
-};
+  import LoginForm from '~/components/molecules/LoginForm.vue'
+  
+  export default {
+    components: {
+      LoginForm
+    },
+
+    layout: 'unauthenticated',
+  };
 </script>
