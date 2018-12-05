94aed50de5eed4b0ed241f730024ab3a00bef5c6
chr <stchr820.6281.kkt@gmail.com>
2018-12-05 13:15:44 +0900

    add description on top page

--- pages/unLoggedIn/index.vue
+++ pages/unLoggedIn/index.vue
@@ -1,6 +1,16 @@
 <template>
-  <div class="hoge">
-    hogehoge
+  <div class="description">
+    <h1>Welcome to Folivora !</h1>
+    <p>面倒くさがりで計画性がないあなたに贈ります。</p>
+    <p>リストが続かない人向けTODOアプリ「Folivora」です。</p>
+    <p>Folivoraは以下の機能であなたのタスク実行をサポートします。</p>
+      <ul>
+        <li>優先度の高いタスクの実行を促す通知をくれます</li>
+        <li>「5分タイマー」でタスクに取り掛かるきっかけを作ります</li>
+        <li>タスクを優先度順に並べ替えます</li>
+        <li>タスクのグループ分けにより計画を支援します</li>
+        <li>他人が公開したタスク計画をフォークできます</li>
+      </ul>
   </div>
 </template>
 
