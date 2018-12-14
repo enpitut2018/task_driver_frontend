8e8ee263d36f160690d1d5c83229051cc80d4e16
itumizu <s1611488@klis.tsukuba.ac.jp>
2018-12-14 23:05:19 +0900

    Change indention level

--- pages/home.vue
+++ pages/home.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
-      <div v-if="isAuthenticated">
-        <TaskBoard :tasks= this.todo :status="TODO"></TaskBoard>
-        <!-- <TaskBoard :tasks= this.doing :status="DOING"></TaskBoard>     -->
-      </div>
+        <div v-if="isAuthenticated">
+            <TaskBoard :tasks= this.todo :status="TODO"></TaskBoard>
+            <!-- <TaskBoard :tasks= this.doing :status="DOING"></TaskBoard>         -->
+        </div>
     </div>
 </template>
 
@@ -12,113 +12,91 @@
     import moment from '~/plugins/moment'
     import TaskBoard from '~/components/organisms/TaskBoard.vue'
 
-  export default {
-      middleware: [
-        'auth',
-      ],
-      data: () => ({
-        user: {}
-      }),
-      components: 
-      {
-        TaskBoard
-      },
-      apollo: {
-        // Query with parameters
-        user: {
-          // gql query
-          query: gql`
-          query($id: ID!){
-            user(id: $id)
-            {
-              groups{
-                id
-                name
-                tasks{
-                  id
-                  name
-                  status
+    export default {
+        middleware: [
+            'auth',
+        ],
+        data: () => ({
+            user: {}
+        }),
+
+        components: 
+        {
+            TaskBoard
+        },
+        apollo: {
+            // Query with parameters
+            user: {
+            // gql query
+                query: gql`
+                    query($id: ID!){
+                        user(id: $id)
+                        {
+                            groups{
+                                id
+                                name
+                                tasks{
+                                    id
+                                    name
+                                    status
+                                }
+                            }
+                        }
+                    }`,
+                // ($id: Int!)
+                // Static parameters
+                variables: {
+                    id: 79
+                },
+            },
+        },
+        computed: {
+            userInfo () {
+                return this.$auth.user
+            },
+            isAuthenticated () {
+                return this.$auth.loggedIn
+            },
+            joinedDate () {
+                let dateRes = this.user.created_at
+                return moment(dateRes, "YYYY-MM-DD HH:mm:ss Z").format("YYYY年M月D日H時m分")
+            },
+
+            todo(){
+                var toString = Object.prototype.toString
+                const todos = []
+                for (var i in this.user.groups) {
+                    for (var n in this.user.groups[i].tasks){
+                        var todo = {}
+                        todo["group"] = this.user.groups[i].name;
+                        todo["name"] = this.user.groups[i].tasks[n].name;
+                        todo["id"] = this.user.groups[i].tasks[n].id;
+
+                        if (this.user.groups[i].tasks[n].status == 0){
+                            todos.push(todo);
+                        }
+                    }
                 }
-              }
+                return todos
+            },
+
+            doing(){
+                // var toString = Object.prototype.toString
+                const todos = []
+                for (var i in this.user.groups) {
+                    for (var n in this.user.groups[i].tasks){
+                        var todo = {}
+                        todo["group"] = this.user.groups[i].name;
+                        todo["name"] = this.user.groups[i].tasks[n].name;
+                        todo["id"] = this.user.groups[i].tasks[n].id;
+
+                        if (this.user.groups[i].tasks[n].status == 0){
+                            todos.push(todo);
+                        }
+                    }
+                }
+                return todos
             }
-          }`,
-          // ($id: Int!)
-          // Static parameters
-          variables: {
-            id: 79
-          },
         },
-      },
-
-      computed: {
-        userInfo () {
-          return this.$auth.user
-        },
-        isAuthenticated () {
-          return this.$auth.loggedIn
-        },
-        joinedDate () {
-          let dateRes = this.user.created_at
-          return moment(dateRes, "YYYY-MM-DD HH:mm:ss Z").format("YYYY年M月D日H時m分")
-        },
-
-        todo(){
-          var toString = Object.prototype.toString
-          const todos = []
-          // var todos = []
-          // var todos = this.user.filter(function(item, index){
-          //   if (item.name == "jiro") return true;
-          // });
-          // todos.push(this.user.groups);
-          // todos.push(toString.call(this.user.groups));
-          // todos.push((this.user.groups).length);
-          for (var i in this.user.groups) {
-            // todos.push(toString.call(this.user.groups[i].tasks));
-            // todos.push(this.user.groups[i].tasks);
-            // todos.push(toString.call(this.user.groups[i].tasks));
-            for (var n in this.user.groups[i].tasks){
-              var todo = {}
-              todo["group"] = this.user.groups[i].name
-              todo["name"] = this.user.groups[i].tasks[n].name
-              todo["id"] = this.user.groups[i].tasks[n].id
-
-              if (this.user.groups[i].tasks[n].status == 0){
-                todos.push(todo)
-                // todos.push(this.user.groups[i].tasks[n]);
-              }
-            }
-          }
-          return todos
-        },
-
-        doing(){
-          var toString = Object.prototype.toString
-          const todos = []
-          // var todos = []
-          // var todos = this.user.filter(function(item, index){
-          //   if (item.name == "jiro") return true;
-          // });
-          // todos.push(this.user.groups);
-          // todos.push(toString.call(this.user.groups));
-          // todos.push((this.user.groups).length);
-          for (var i in this.user.groups) {
-            // todos.push(toString.call(this.user.groups[i].tasks));
-            // todos.push(this.user.groups[i].tasks);
-            // todos.push(toString.call(this.user.groups[i].tasks));
-            for (var n in this.user.groups[i].tasks){
-              var todo = {}
-              todo["group"] = this.user.groups[i].name
-              todo["name"] = this.user.groups[i].tasks[n].name
-              todo["id"] = this.user.groups[i].tasks[n].id
-
-              if (this.user.groups[i].tasks[n].status == 0){
-                todos.push(todo)
-                // todos.push(this.user.groups[i].tasks[n]);
-              }
-            }
-          }
-          return todos
-        }
-      },
-  };
+    };
 </script>
