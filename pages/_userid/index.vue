7cc725a40a2ecfa42bccbdcad235ec34043127a3
kunado <katsusand1@gmail.com>
2018-12-19 10:44:58 +0900

    update task page

---
+++ pages/_userid/index.vue
@@ -0,0 +1,36 @@
+<template>
+	<div>
+		<h1 class="groupName">{{groupid}}</h1>
+		<div class="boards">
+			<TaskBoard/>
+		</div>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+	.groupName {
+		font-size: 35px;
+		font-weight: 700;
+		margin: 25px 0 10px 0;
+	}
+	.boards {
+		width: 100%;
+		display: flex;
+	}
+</style>
+
+<script>
+	import TaskBoard from '~/components/organisms/TaskBoard.vue'
+
+	export default {
+		data() {
+			return {
+				userid: this.$route.params.userid,
+				groupid: this.$route.params.groupid
+			}
+		},
+		components: {
+			TaskBoard
+		}
+	}
+</script>
